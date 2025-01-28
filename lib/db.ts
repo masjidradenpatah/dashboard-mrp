9836e44771edbc19073db4d0f5eec1d78e4d9302
ArziTech <wawcam123@gmail.com>
2025-01-28 13:56:49 +0700

    fixing edge compability

--- lib/db.ts
+++ lib/db.ts
@@ -1,17 +1,7 @@
-
-import { Pool } from 'pg'
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '@prisma/client'
-
-const connectionString = `${process.env.DATABASE_URL}`
-
-const pool = new Pool({ connectionString })
-const adapter = new PrismaPg(pool)
-// const prisma = new PrismaClient({ adapter })
+import { PrismaClient } from "@prisma/client"
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({adapter})
+export const prisma = globalForPrisma.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV !== "production")
-  globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
